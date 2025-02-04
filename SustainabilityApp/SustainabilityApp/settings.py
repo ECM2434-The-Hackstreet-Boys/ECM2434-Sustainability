f440a4259c3e2a6767c7e76ba69f0ead23c59b3a
Edward Pratt <41444303+Edward-Pratt@users.noreply.github.com>
2025-02-04 18:08:33 +0000

    IP Address Changes

--- SustainabilityApp/SustainabilityApp/settings.py
+++ SustainabilityApp/SustainabilityApp/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['130.162.180.61', '127.0.0.1']
+ALLOWED_HOSTS = ['130.162.180.61', '127.0.0.1', 'www.edward-pratt.live', 'edward-pratt.live']
 
 
 # Application definition
