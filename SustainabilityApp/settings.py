e96234c5b196870f9fddd492fdb53fba25751e44
Edward Pratt <41444303+Edward-Pratt@users.noreply.github.com>
2025-02-06 03:11:39 +0000

    Filepath Changes

--- SustainabilityApp/settings.py
+++ SustainabilityApp/settings.py
@@ -141,6 +141,10 @@ TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, '/templates'),
 )
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
