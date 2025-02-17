fa26774ddbed55ba888d06f2ce0afb76ab3338b2
CliffHanger201 <sandy.hay@ntlworld.com>
2025-02-17 02:21:05 +0000

    Moving files QR scanner changes to branch instead of main, apologies :/

--- apps/qrscanner/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import QRCodeScan, QRCodeView
-
-urlpatterns = [
-    path('qrcode-generate', QRCodeView.as_view(), name='qrcode'),
-    path('qrcode-scan', QRCodeScan.as_view(), name='qrscan'),
-]
