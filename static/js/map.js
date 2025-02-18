db2f0a6c16a43e40f7c5cc0c54b42d9f76189fc4
MattMcCree01 <matthewamccree@gmail.com>
2025-02-18 23:26:10 +0000

    basic geo stuff

--- static/js/map.js
+++ static/js/map.js
@@ -8,54 +8,30 @@ function initializeMap(iconUrl) {
 
         // Custom icon
         var customIcon = L.icon({
-            iconUrl: "/static/resources/blue-circle-noBg.png", // Update the path to your icon
+            iconUrl: iconUrl, // Update the path to your icon
             iconSize: [38, 38], // Size of the icon
             iconAnchor: [19, 19], // Point of the icon which will correspond to marker's location
             popupAnchor: [0, -19] // Point from which the popup should open relative to the iconAnchor
         });
 
-        // Initial markers with custom icon
-        const forumMarker = L.marker([50.73554, -3.534], {icon: customIcon, opacity: 0.5}).addTo(map).bindPopup("The Forum");
-        const resilienceMarker = L.marker([50.73694, -3.53301], {icon: customIcon, opacity: 0.5}).addTo(map).bindPopup("Centre for Resilience");
-
         // Variable to store the user's location marker
         let userMarker = null;
+        let userLocation = null;
 
         map.on('locationfound', function(e) {
-            const userLocation = e.latlng;
+            userLocation = e.latlng;
+            console.log(userLocation);
 
             // Remove previous user marker if it exists
             if (userMarker) {
                 map.removeLayer(userMarker);
+            } else {
+                map.setView(userLocation);
             }
 
             // Add new user marker with 50% opacity and custom icon
             userMarker = L.marker(userLocation, {icon: customIcon, opacity: 0.5}).addTo(map)
                 .bindPopup("You are within " + Math.round(e.accuracy) + " meters from this point").openPopup();
-
-            // Calculate the distance to the forum marker
-            const forumDistance = userLocation.distanceTo(forumMarker.getLatLng());
-
-            // Change the opacity and popup content based on distance to the forum marker
-            if (forumDistance < 50) {
-                forumMarker.setOpacity(1);
-                forumMarker.bindPopup("You are nearby the Forum").openPopup();
-            } else {
-                forumMarker.setOpacity(0.5);
-                forumMarker.bindPopup("The Forum").openPopup();
-            }
-
-            // Calculate the distance to the resilience marker
-            const resilienceDistance = userLocation.distanceTo(resilienceMarker.getLatLng());
-
-            // Change the opacity and popup content based on distance to the resilience marker
-            if (resilienceDistance < 50) {
-                resilienceMarker.setOpacity(1);
-                resilienceMarker.bindPopup("You are nearby the Centre for Resilience").openPopup();
-            } else {
-                resilienceMarker.setOpacity(0.5);
-                resilienceMarker.bindPopup("Centre for Resilience").openPopup();
-            }
         });
 
         // Fix issue where map does not render fully
@@ -63,12 +39,22 @@ function initializeMap(iconUrl) {
             map.invalidateSize();
         }, 500);
 
-        map.locate({setView: true, maxZoom: 16, watch: true, enableHighAccuracy: true});
+        map.locate({ maxZoom: 16, watch: true, enableHighAccuracy: true });
 
         function onLocationError() {
             alert("Location access denied. Please enable location services.");
         }
 
         map.on('locationerror', onLocationError);
+
+        // Add event listener for recenter button
+        document.getElementById('recenter-button').addEventListener('click', function() {
+            if (userLocation) {
+                map.setView(userLocation, 16);
+                console.log("Map recentered on user's location");
+            } else {
+                alert("User location not found. Please enable location services.");
+            }
+        });
     });
 }
