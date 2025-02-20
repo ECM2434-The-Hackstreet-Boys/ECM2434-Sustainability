2776ef9e8bf61bde7e621478f963ff3ec068a147
Edward Pratt <41444303+Edward-Pratt@users.noreply.github.com>
2025-02-20 14:28:34 +0000

    Update views.py

--- apps/garden/views.py
+++ apps/garden/views.py
@@ -1,3 +1,13 @@
+import json
+import os
+
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+
+from SustainabilityApp import settings
+from .models import Garden
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
