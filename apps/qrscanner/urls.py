7d444ff06d524ded088d06a746c205fdefa4d5f9
Edward Pratt <edwardgwpratt@gmail.com>
2025-02-21 18:41:46 +0000

    Remove QR Scanner Disaster

--- apps/qrscanner/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import QRCodeScan, QRCodeView
-
-urlpatterns = [
-    path('qr-generate/', QRCodeView.as_view(), name='qrcode'),
-    path('qr-scan/', QRCodeScan.as_view(), name='qrscan'),
-]
