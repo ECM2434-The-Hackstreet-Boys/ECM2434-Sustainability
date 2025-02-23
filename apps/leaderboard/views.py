cc0a1912e3935e6a8d47a9cb76d8fd762fd413a2
Edward Pratt <edwardgwpratt@gmail.com>
2025-02-23 15:15:44 +0000

    Start of Leaderboard

--- apps/leaderboard/views.py
+++ apps/leaderboard/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+import apps.stats.models
+
+
 # Create your views here.
 @login_required
 def leaderboardpage(request):
 
     return render(request, "leaderboard.html")
+
+def retrieveStats(request):
+    records = apps.stats.models.Stats.objects.all()
+    return render(request, "leaderboard.html", {'records': records})
