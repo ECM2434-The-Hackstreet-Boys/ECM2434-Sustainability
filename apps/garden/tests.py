018e1c5833016af8395bbc2549e223c39daf954c
CliffHanger201 <sandy.hay@ntlworld.com>
2025-02-23 21:50:22 +0000

    Garden Testing update v2

--- apps/garden/tests.py
+++ apps/garden/tests.py
@@ -1,6 +1,8 @@
-from django.test import TestCase
+import json
+from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+from .models import Garden
 
 User = get_user_model()
 
@@ -24,36 +26,16 @@ class GardenPageTests(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('login') + '?next=' + reverse('garden')) # Check if the user is redirected to the login page
 
+        # Create a test garden
+
+class SaveGardenTest(TestCase):
+    def setUp(self):
+        # Create a user to test saving functionality
+        self.user = User.objects.create_user(username='testuser', password='testpassword#123')
+        self.client = Client()
+        self.client.login(username='testuser', password='')
 class GardenLoadTests(TestCase):
     def setUp(self):
-        # Create a user to test logged-in behaviour
+        # Create a user to test loading functionality
         self.user = User.objects.create_user(username='testuser', password='testpassword#123')
-
-    def test_load_garden_logged_in(self):
-        # Test if the load garden endpoint is accessible when logged in
-        self.client.login(username='testuser', password='testpassword#123') # Log in with the created user
-        response = self.client.get(reverse('load_garden')) # Make a GET request to the load garden endpoint
-        self.assertEqual(response.status_code, 200) # Check if the status code is 200 (OK)
-
-    def test_load_garden_logged_out(self):
-        # Test if the load garden endpoint is inaccessible when logged out
-        response = self.client.get(reverse('load_garden'))
-        self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, reverse('login') + '?next=' + reverse('load_garden')) # Check if the user is redirected to the login page
-
-class GardenSaveTests(TestCase):
-    def setUp(self):
-        # Create a user to test logged-in behaviour
-        self.user = User.objects.create_user(username='testuser', password='testpassword#123')
-
-    def test_save_garden_logged_in(self):
-        # Test if the save garden endpoint is accessible when logged in
-        self.client.login(username='testuser', password='testpassword#123') # Log in with the created user
-        response = self.client.post(reverse('save_garden')) # Make a POST request to the save garden endpoint
-        self.assertEqual(response.status_code, 200) # Check if the status code is 200 (OK)
-
-    def test_save_garden_logged_out(self):
-        # Test if the save garden endpoint is inaccessible when logged out
-        response = self.client.post(reverse('save_garden'))
-        self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, reverse('login') + '?next=' + reverse('save_garden')) # Check if the user is redirected to the login page
+    
