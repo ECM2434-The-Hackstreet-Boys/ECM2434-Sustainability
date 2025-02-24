ebce3f62a8f163980a5193e436fc1ee21cbdce61
MattMcCree01 <matthewamccree@gmail.com>
2025-02-24 14:44:36 +0000

    Added some hardcoded locations as well as a test location, links to quiz page if close enough.

--- static/js/play_screen.js
+++ static/js/play_screen.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // will open a qr scanner in the future
     document.getElementById('my-button').addEventListener('click', function () {
         alert("Open Camera button clicked!");
     });
 });
+
+// Function to handle the quiz link click event
+function quizClicked(e) {
+    console.log(e.detail.message);
+    // Redirect to the quiz page
+    window.location.assign("/quiz");
+}
+
+// Add event listener for the custom event
+document.addEventListener('quizLinkClicked', quizClicked);
