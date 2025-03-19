163d0bfe2de1a60d0c49f9552bf3e4615a48f1b7
EthanClapham <160736870+EthanClapham@users.noreply.github.com>
2025-03-19 20:56:20 +0000

    Page styled and elements added

--- apps/stats/views.py
+++ apps/stats/views.py
@@ -1,11 +1,23 @@
-# Author: Edward Pratt
+# Author: Edward Pratt, Ethan Clapham
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Stats
+from ..accounts.models import CustomUser
 
 # Create your views here.
 
 # View for the stats app /stats/
 @login_required
 def statspage(request):
+    user = CustomUser.objects.get(id=request.user.id)
+    stats, created = Stats.objects.get_or_create(userID=user)
 
-    return render(request, "statistics.html")
+    content = {
+        'user_points' : stats.yourPoints,
+        'packaging_recycled' : stats.packagingRecycled,
+        'plastic_recycled' : stats.plasticRecycled,
+        'metal_recycled' : stats.metalRecycled,
+        'paper_recycled' : stats.paperRecycled,
+    }
+
+    return render(request, "statistics.html", content)
